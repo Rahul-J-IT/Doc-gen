aa16f90a71a7644617db9166a76b52262730d0cc
Rahul-J-IT <rahul22it203@gmail.com>
2025-01-06 13:38:29 +0530

    script

--- backend/routes/odRoutes.js
+++ backend/routes/odRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const odController = require('../controllers/odController');
+const { isAuthenticatedUser } = require('../middlewares/authenticate');
 
 // Student routes
-router.post('/od', odController.createOD);
+router.post('/od',isAuthenticatedUser, odController.createOD);
 router.get('/myods', odController.getMyODs);
 router.get('/od/:id', odController.getSingleODUser);
 router.put('/od/:id', odController.updateODUser);
@@ -16,3 +17,5 @@ router.put('/admin/od/:id', odController.updateODAdmin);
 router.delete('/admin/od/:id', odController.deleteOD);
 
 module.exports = router;
+
+
